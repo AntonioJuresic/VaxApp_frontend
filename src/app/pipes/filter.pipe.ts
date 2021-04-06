33828c6a0b106273c2e34ce02da6098c935331e9
AntonioJuresic <ajuresic@tvz.hr>
2021-04-06 16:31:45 +0200

    3. Labos - Dodao sam filter po imenu. :)

---
+++ src/app/pipes/filter.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(items: any[], field: string, value: string): any[] {
+    if (!items || !value || !field) {
+      return items;
+    }
+    return items.filter(item => item[field].toLowerCase().includes(value.toLowerCase()));
+  }
+
+}
