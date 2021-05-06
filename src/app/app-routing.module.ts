b246f64ce1e04973915235d2f239a389b81d568f
AntonioJuresic <ajuresic@tvz.hr>
2021-05-06 18:51:15 +0200

    6. Labos - Ispravak bugova.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SideEffectByResearchNameComponent } from './side-effect-by-research-name/side-effect-by-research-name.component';
 import { SideEffectByShortDescriptionComponent } from './side-effect-by-short-description/side-effect-by-short-description.component';
-import { SideEffectDetailComponent } from './side-effect-detail/side-effect-detail.component';
 import { SideEffectsComponent } from './side-effects/side-effects.component';
 import { VaccineDetailComponent } from './vaccine-detail/vaccine-detail.component';
 import { VaccinesComponent } from './vaccines/vaccines.component';
@@ -11,8 +10,7 @@ const routes: Routes = [
     { path: "vaccines", component: VaccinesComponent },
     { path: "vaccines/:researchName", component: VaccineDetailComponent },
     { path: "sideeffects", component: SideEffectsComponent },
-    { path: "sideeffects/search", component: SideEffectByShortDescriptionComponent},
-    { path: "sideeffects/detail", component: SideEffectDetailComponent}
+    { path: "sideeffects/search", component: SideEffectByShortDescriptionComponent}
 
 ];
 
